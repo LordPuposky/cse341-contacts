2193510a9a292fe9f29084c7fa8cb97e38ba8a8e
Yesid Romero <augustomero@gmail.com>
2026-01-13 21:23:58 -0500

    Fix: swagger connection

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -27,7 +27,7 @@ const createContact = async (req, res) => {
         favoriteColor: req.body.favoriteColor,
         birthday: req.body.birthday
     };
-    const response = await mongodb.getDb().db().collection('contacts').insertOne(contact);
+    const response = await mongodb.getDb().db('contacts_db').collection('contacts').insertOne(contact);
     if (response.acknowledged) {
         res.status(201).json(response.insertedId);
     } else {
